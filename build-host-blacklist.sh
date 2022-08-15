53157fcb80c78c1563f97fc70a7844daf235ed46
KFC <digitalexmachina+gitkraken@gmail.com>
2022-08-16 00:58:53 +0200

    initial commit

---
+++ build-host-blacklist.sh
@@ -0,0 +1,21 @@
+TARGET_DIR="./hosts-list/download"
+
+rm -Rf $TARGET_DIR
+mkdir $TARGET_DIR
+
+echo "Downloading blacklist into $TARGET_DIR"
+# Update lists directly from repos
+curl -o $TARGET_DIR/adservers.txt https://raw.githubusercontent.com/anudeepND/blacklist/master/adservers.txt || exit 1
+#curl -o $TARGET_DIR/facebook-extended.txt https://www.github.developerdan.com/hosts/lists/facebook-extended.txt || exit 1
+curl -o $TARGET_DIR/coinminers-all.txt https://gitlab.com/ZeroDot1/CoinBlockerLists/-/raw/master/list.txt?inline=false || exit 1
+
+# Merge
+echo "Creating new host file"
+rm ./hosts
+touch ./hosts
+cat ./hosts-list/*.txt $TARGET_DIR/*.txt > ./hosts
+
+# replace machine host
+echo "Replacing /etc/hosts"
+mv ./hosts /etc/hosts
+echo "Done!"
